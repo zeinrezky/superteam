a777d3c12eae058584ed5d89ba86fdaa26816cfb
David Cornelius T <davidcornelius15@gmail.com>
2018-07-10 13:53:50 +0700

    First Commit David

---
+++ SuperTeam/David Cornelius/DavidViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DavidViewController.swift
+//  SuperTeam
+//
+//  Created by David Cornelius on 10/07/18.
+//  Copyright © 2018 Zein. All rights reserved.
+//
+
+import UIKit
+
+class DavidViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
